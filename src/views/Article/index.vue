5136d0a9fa010ebe72c61b4e2062940bc873c201
zhangsan <2011358693@qq.com>
2021-05-10 10:51:01 +0800

    完成图片预览

--- src/views/Article/index.vue
+++ src/views/Article/index.vue
@@ -22,7 +22,11 @@
         round
       >{{aryicleDetail.is_followed ? '已关注' : '关注'}}</van-button>
     </van-cell>
-    <div class="markdown-body" v-html="aryicleDetail.content"></div>
+    <div
+      ref="article-concent"
+      class="markdown-body"
+      v-html="aryicleDetail.content"
+    ></div>
   </div>
 </template>
 
@@ -30,6 +34,10 @@
 import '../../styles/mackdown.css'
 
 import { getArticleDetail } from '../../api/article'
+
+//  引入 ImagePreview 用于函数式调用预览图片
+import { ImagePreview } from 'vant'
+
 export default {
   props: {
     // 通过动态路由映射过来的 props 参数
@@ -51,6 +59,34 @@ export default {
     async getArticle () {
       const { data } = await getArticleDetail(this.articleId)
       this.aryicleDetail = data
+
+      // ! 由于vue的视图更新是异步操作，如果需要在更新完成之后，获取 Dom 节点的话，直接获取是无法获取成功的
+      // ! 必须借助与 $nextTick 方法，该方法会在延迟到下次 dom 更新结束之后再调用
+      // ! 此时就可以获取到所有的img标签
+      this.$nextTick(() => {
+        // 调用图片预览函数
+        this.PreviewImg()
+      })
+    },
+    // 用于预览图片
+    PreviewImg () {
+      // 获取文章详情容器
+      const articleConcent = this.$refs['article-concent']
+      // 获取当前容器里面所有的img标签
+      const imgs = articleConcent.querySelectorAll('img')
+      // 用于存储图片路径
+      const imgPath = []
+      imgs.forEach((img, i) => {
+        imgPath.push(img.src)
+        // 给imgs绑定点击事件
+        img.onclick = () => {
+          // 调用预览图片函数
+          ImagePreview({
+            images: imgPath,
+            startPosition: i
+          })
+        }
+      })
     }
   }
 }
