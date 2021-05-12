3380a2e459b3c71940b4314f587a3d9f2f449437
zhangsan <2011358693@qq.com>
2021-05-12 16:58:19 +0800

    使用axios响应拦截器统一处理错误

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,6 +6,8 @@ import jsonBig from 'json-bigint'
 // 导入store
 import store from '../store/index'
 
+import { Toast } from 'vant'
+
 // 创建axios实例对象
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/',
@@ -31,10 +33,30 @@ request.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${userInfo.token}`
   }
   return config
+}, (err) => {
+  console.log(err)
 })
 
 // 响应拦截器
-request.interceptors.response.use(config => config.data)
+request.interceptors.response.use(config => config.data,
+  // 在此处统一处理响应错误
+  err => {
+    console.dir(err)
+    const status = err.response.status
+    if (status === 400) {
+      // 客户端参数错误
+      Toast.fail('请求参数错误')
+    } else if (status === 401) {
+      // token 错误
+    } else if (status === 403) {
+      // 没有权限
+      Toast.fail('没有权限')
+    } else if (status >= 500) {
+      // 服务器端异常
+      Toast.fail('服务器端异常，请稍后重试')
+    }
+  }
+)
 
 // 导出
 export default request
